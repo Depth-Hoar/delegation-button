99a3f3f4ace1a84f6dc21bdb0c87dc4fdba6e4f0
Adam <44712760+Depth-Hoar@users.noreply.github.com>
2022-02-08 10:13:40 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import logo from './edgeware-main-logomark.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <p>
+          Edgeware Delegation Interface
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <p>
+          Coming soon!
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
